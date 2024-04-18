d21e45f5eac57343c5a907a14564b25557ebfee5
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-04-18 10:48:40 +0200

    Improved classify_iris with over 90% accuracy

--- Lab3/ex1.py
+++ Lab3/ex1.py
@@ -6,18 +6,23 @@ df = pd.read_csv("iris.csv")
 (train_set, test_set) = train_test_split(df.values, train_size=0.7, random_state=288493)
 
 def classify_iris(sl, sw, pl, pw):
-    if sl > 4:
+    if pw < 1 and pl < 2:
         return("Setosa")
-    elif pl <= 5:
+    elif sl >= 6 and pl >= 5:
         return("Virginica")
     else:
         return("Versicolor")
 
 good_predictions = 0
 len = test_set.shape[0]
+print(train_set)
+# print(test_set)
 for i in range(len):
-    sl, sw, pl, pw = train_set[:, 0:4][i][0], train_set[:, 0:4][i][1], train_set[:, 0:4][i][2], train_set[:, 0:4][i][3]
-    if classify_iris(sl, sw, pl, pw) == test_set[:, 4][i]:
+    print('kolejny')
+    sl, sw, pl, pw = test_set[i][:4]
+    true_class = test_set[i][4]
+    print(classify_iris(sl, sw, pl, pw), sl, sw, pl, pw, true_class)
+    if classify_iris(sl, sw, pl, pw) == true_class:
         good_predictions = good_predictions + 1
         print(good_predictions)
         print(good_predictions/len*100, "%")
