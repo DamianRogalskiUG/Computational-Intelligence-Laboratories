b23614a764fd384e72445b52ba9b4e817a08671a
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-05-31 00:48:34 +0200

    Ex 3 finished with acceptible accuracy

---
+++ Lab6/ex3.py
@@ -0,0 +1,38 @@
+import os
+import cv2
+
+
+def count_birds(image):
+    # Converting into gray scale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Looking for edges
+    edges = cv2.Canny(gray, 50, 150)
+
+    # Morphology
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+    # Looking for contours
+    contours, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Counting birds
+    bird_count = len(contours)
+
+    return bird_count
+
+
+# Find and process all images
+def process_images_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(folder_path, filename)
+            image = cv2.imread(image_path)
+            bird_count = count_birds(image)
+            print(f"Obraz: {filename}, Liczba ptaków: {bird_count}")
+
+
+# Path to the folder
+folder_path = "bird_miniatures"
+
+process_images_in_folder(folder_path)
